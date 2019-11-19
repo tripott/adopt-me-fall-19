a3ce1bf97f05ac60dbe4e601a5902325b2bec585
harvest <wottinger@trhc.com>
2019-11-19 10:19:42 -0500

    new pet browserlist displayed results

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import pet, { ANIMALS } from '@frontendmasters/pet'
 import useDropdown from './useDropdown'
+import Results from './Results'
 
 const SearchParams = () => {
   const [location, setLocation] = useState('Seattle, WA')
@@ -8,7 +9,21 @@ const SearchParams = () => {
   // const [breed, setBreed] = useState('')
   const [breeds, setBreeds] = useState([])
   const [animal, , AnimalDropdown] = useDropdown('Animal', 'dog', ANIMALS)
-  const [, setBreed, BreedDropdown] = useDropdown('Breed', '', breeds)
+  const [breed, setBreed, BreedDropdown] = useDropdown('Breed', '', breeds)
+  const [pets, setPets] = useState([])
+
+  // async/await - async function returns a promise that will resolve
+  //  when the function call completes
+  // Inside async functions, you can use await keyword
+  //  pet.animals returns a promise.
+  //  await says "wait until the call to pet.animals completes and give me the data"
+  //   so we await for the return of the data object..from which we
+  //    destructure the animals property then setPets with the animals.
+
+  async function requestPets() {
+    const { animals } = await pet.animals({ location, breed, type: animal })
+    setPets(animals || [])
+  }
 
   useEffect(() => {
     setBreeds([])
@@ -24,20 +39,28 @@ const SearchParams = () => {
 
   return (
     <div className="search-params">
-      <label htmlFor="location">
-        location
-        <input
-          id="location"
-          value={location}
-          placeholder="Please enter location"
-          onChange={e => setLocation(e.target.value)}
-        />
-      </label>
+      <form
+        onSubmit={e => {
+          e.preventDefault()
+          requestPets()
+        }}
+      >
+        <label htmlFor="location">
+          location
+          <input
+            id="location"
+            value={location}
+            placeholder="Please enter location"
+            onChange={e => setLocation(e.target.value)}
+          />
+        </label>
 
-      <AnimalDropdown />
-      <BreedDropdown />
+        <AnimalDropdown />
+        <BreedDropdown />
 
-      <button>Submit</button>
+        <button>Submit</button>
+      </form>
+      <Results pets={pets} />
     </div>
   )
 }
